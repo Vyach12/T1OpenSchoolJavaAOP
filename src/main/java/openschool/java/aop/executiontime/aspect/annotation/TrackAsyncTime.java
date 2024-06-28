b003f4d61610a71f31bd9e66bf3684333e3fb33b
Vyach12 <slava.gusarov.73@mail.ru>
2024-06-28 14:02:06 +0300

    [doc]: added javadoc for annotations

--- src/main/java/openschool/java/aop/executiontime/aspect/annotation/TrackAsyncTime.java
+++ src/main/java/openschool/java/aop/executiontime/aspect/annotation/TrackAsyncTime.java
@@ -1,10 +1,32 @@
 package openschool.java.aop.executiontime.aspect.annotation;
 
+import openschool.java.aop.executiontime.aspect.TrackingAspect;
+import openschool.java.aop.executiontime.aspect.TrackingAsyncAspect;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация, указывающая, что время выполнения аннотированного метода должно быть отслежено.
+ * Эта аннотация используется для пометки методов, время выполнения которых должно быть отслежено и записано асинхронно.
+ * Аннотация применяется только к методам, а собранные данные будут включать имя класса,
+ * имя метода и продолжительность выполнения метода.
+ *
+ * <p>Пример использования:</p>
+ * <pre>
+ * {@code
+ * @TrackAsyncTime
+ * public void exampleMethod() {
+ *     // Реализация метода
+ * }
+ * }
+ * </pre>
+ *
+ * @see TrackTime
+ * @see TrackingAsyncAspect
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface TrackAsyncTime {
--- src/main/java/openschool/java/aop/executiontime/aspect/annotation/TrackTime.java
+++ src/main/java/openschool/java/aop/executiontime/aspect/annotation/TrackTime.java
@@ -1,10 +1,31 @@
 package openschool.java.aop.executiontime.aspect.annotation;
 
+import openschool.java.aop.executiontime.aspect.TrackingAspect;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация, указывающая, что время выполнения аннотированного метода должно быть отслежено.
+ * Эта аннотация используется для пометки методов, время выполнения которых должно быть отслежено и записано.
+ * Аннотация применяется только к методам, а собранные данные будут включать имя класса,
+ * имя метода и продолжительность выполнения метода.
+ *
+ * <p>Пример использования:</p>
+ * <pre>
+ * {@code
+ * @TrackTime
+ * public void exampleMethod() {
+ *     // Реализация метода
+ * }
+ * }
+ * </pre>
+ *
+ * @see TrackAsyncTime
+ * @see TrackingAspect
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface TrackTime {
